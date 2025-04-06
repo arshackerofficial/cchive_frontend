26e407bc3fcbb7a5c16c7578c8ca8cb65ab3cc8c
amrxt1 <amrxt11@outlook.com>
2025-04-06 11:26:16 -0700

    Begin Speedrun: Create api and AuthContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from '../lib/api';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const checkUser = async () => {
+            try {
+                const res = await api.get('/auth/validate_token');
+                setUser(res.data.data);
+            } catch (err) {
+                console.log(err);
+                setUser(null);
+            }
+        }
+        checkUser();
+    }, [])
+
+    const login = async (email, password) => {
+        const res = await api.post('/auth/sign_in', {email, password});
+        setUser(res.data.data);
+        return res.data.data;
+    }
+    const register = async (email, password) => {
+        const res = await api.post('/auth', {email, password});
+        setUser(res.data.data);
+        return res.data.data;
+    }
+    const logout = async () => {
+        await api.delete('/auth/sign_out');
+        localStorage.clear();
+        setUser(null);
+    }
+
+    return(
+        <AuthContext.Provider value={{user, login, register, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
---
+++ src/lib/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: "https://localhost:3000/api/v1"
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access-token');
+    const client = localStorage.getItem('client');
+    const uid = localStorage.getItem('uid');
+
+    if (token && client && uid){
+        config.headers['access-token'] = token
+        config.headers['client'] = client
+        config.headers['uid'] = uid
+    }
+    return config;
+})
+
+api.interceptors.response.use((response) => {
+    const token = response.headers['access-token'];
+    if (token){
+        localStorage.setItem('access-token', token);
+        localStorage.setItem('client',response.headers['client']);
+        localStorage.setItem('uid', response.headers['uid']);
+    }
+    return response;
+}, (error) => Promise.reject(error));
+
+export default api;
